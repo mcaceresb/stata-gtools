2b159e4788295d10f8dfcbf893cd81de4e6a7e1c
Mauricio Caceres <mauricio.caceres.bravo@gmail.com>
2019-06-12 18:56:46 -0400

    Added tests for issue 60

---
+++ src/github-issues/60/plugin/test.do
@@ -0,0 +1,6 @@
+clear all
+program test1, plugin using(test1.plugin)
+plugin call test1
+
+program test2, plugin using(test2.plugin)
+plugin call test2
---
+++ src/github-issues/60/plugin/test1.c
@@ -0,0 +1,17 @@
+#include "stplugin.h"
+
+int main()
+{
+    return(0);
+}
+
+int WinMain()
+{
+    return(0);
+}
+
+STDLL stata_call(int argc, char *argv[])
+{
+    SF_display("Hello World\n") ;
+    return(0) ;
+}
